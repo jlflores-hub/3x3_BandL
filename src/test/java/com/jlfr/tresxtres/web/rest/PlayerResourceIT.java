936e04c838b1ee154b01268a642c108df6506425
Jose Luis Flores Roman <jlfloresroman@gmail.com>
2021-08-04 18:01:54 +0200

    create entity Player

---
+++ src/test/java/com/jlfr/tresxtres/web/rest/PlayerResourceIT.java
@@ -0,0 +1,254 @@
+package com.jlfr.tresxtres.web.rest;
+
+import com.jlfr.tresxtres.TresXtresApp;
+import com.jlfr.tresxtres.domain.Player;
+import com.jlfr.tresxtres.domain.User;
+import com.jlfr.tresxtres.repository.PlayerRepository;
+import com.jlfr.tresxtres.service.PlayerService;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * Integration tests for the {@link PlayerResource} REST controller.
+ */
+@SpringBootTest(classes = TresXtresApp.class)
+
+@AutoConfigureMockMvc
+@WithMockUser
+public class PlayerResourceIT {
+
+    @Autowired
+    private PlayerRepository playerRepository;
+
+    @Autowired
+    private PlayerService playerService;
+
+    @Autowired
+    private EntityManager em;
+
+    @Autowired
+    private MockMvc restPlayerMockMvc;
+
+    private Player player;
+
+    /**
+     * Create an entity for this test.
+     *
+     * This is a static method, as tests for other entities might also need it,
+     * if they test an entity which requires the current entity.
+     */
+    public static Player createEntity(EntityManager em) {
+        Player player = new Player();
+        // Add required entity
+        User user = UserResourceIT.createEntity(em);
+        em.persist(user);
+        em.flush();
+        player.setUser(user);
+        return player;
+    }
+    /**
+     * Create an updated entity for this test.
+     *
+     * This is a static method, as tests for other entities might also need it,
+     * if they test an entity which requires the current entity.
+     */
+    public static Player createUpdatedEntity(EntityManager em) {
+        Player player = new Player();
+        // Add required entity
+        User user = UserResourceIT.createEntity(em);
+        em.persist(user);
+        em.flush();
+        player.setUser(user);
+        return player;
+    }
+
+    @BeforeEach
+    public void initTest() {
+        player = createEntity(em);
+    }
+
+    @Test
+    @Transactional
+    public void createPlayer() throws Exception {
+        int databaseSizeBeforeCreate = playerRepository.findAll().size();
+
+        // Create the Player
+        restPlayerMockMvc.perform(post("/api/players")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(TestUtil.convertObjectToJsonBytes(player)))
+            .andExpect(status().isCreated());
+
+        // Validate the Player in the database
+        List<Player> playerList = playerRepository.findAll();
+        assertThat(playerList).hasSize(databaseSizeBeforeCreate + 1);
+        Player testPlayer = playerList.get(playerList.size() - 1);
+
+        // Validate the id for MapsId, the ids must be same
+        assertThat(testPlayer.getId()).isEqualTo(testPlayer.getUser().getId());
+    }
+
+    @Test
+    @Transactional
+    public void createPlayerWithExistingId() throws Exception {
+        int databaseSizeBeforeCreate = playerRepository.findAll().size();
+
+        // Create the Player with an existing ID
+        player.setId(1L);
+
+        // An entity with an existing ID cannot be created, so this API call must fail
+        restPlayerMockMvc.perform(post("/api/players")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(TestUtil.convertObjectToJsonBytes(player)))
+            .andExpect(status().isBadRequest());
+
+        // Validate the Player in the database
+        List<Player> playerList = playerRepository.findAll();
+        assertThat(playerList).hasSize(databaseSizeBeforeCreate);
+    }
+
+    @Test
+    @Transactional
+    public void updatePlayerMapsIdAssociationWithNewId() throws Exception {
+        // Initialize the database
+        playerService.save(player);
+        int databaseSizeBeforeCreate = playerRepository.findAll().size();
+
+        // Add a new parent entity
+        User user = UserResourceIT.createEntity(em);
+        em.persist(user);
+        em.flush();
+
+        // Load the player
+        Player updatedPlayer = playerRepository.findById(player.getId()).get();
+        // Disconnect from session so that the updates on updatedPlayer are not directly saved in db
+        em.detach(updatedPlayer);
+
+        // Update the User with new association value
+        updatedPlayer.setUser(user);
+
+        // Update the entity
+        restPlayerMockMvc.perform(put("/api/players")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(TestUtil.convertObjectToJsonBytes(updatedPlayer)))
+            .andExpect(status().isOk());
+
+        // Validate the Player in the database
+        List<Player> playerList = playerRepository.findAll();
+        assertThat(playerList).hasSize(databaseSizeBeforeCreate);
+        Player testPlayer = playerList.get(playerList.size() - 1);
+
+        // Validate the id for MapsId, the ids must be same
+        // Uncomment the following line for assertion. However, please note that there is a known issue and uncommenting will fail the test.
+        // Please look at https://github.com/jhipster/generator-jhipster/issues/9100. You can modify this test as necessary.
+        // assertThat(testPlayer.getId()).isEqualTo(testPlayer.getUser().getId());
+    }
+
+    @Test
+    @Transactional
+    public void getAllPlayers() throws Exception {
+        // Initialize the database
+        playerRepository.saveAndFlush(player);
+
+        // Get all the playerList
+        restPlayerMockMvc.perform(get("/api/players?sort=id,desc"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+            .andExpect(jsonPath("$.[*].id").value(hasItem(player.getId().intValue())));
+    }
+    
+    @Test
+    @Transactional
+    public void getPlayer() throws Exception {
+        // Initialize the database
+        playerRepository.saveAndFlush(player);
+
+        // Get the player
+        restPlayerMockMvc.perform(get("/api/players/{id}", player.getId()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+            .andExpect(jsonPath("$.id").value(player.getId().intValue()));
+    }
+
+    @Test
+    @Transactional
+    public void getNonExistingPlayer() throws Exception {
+        // Get the player
+        restPlayerMockMvc.perform(get("/api/players/{id}", Long.MAX_VALUE))
+            .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @Transactional
+    public void updatePlayer() throws Exception {
+        // Initialize the database
+        playerService.save(player);
+
+        int databaseSizeBeforeUpdate = playerRepository.findAll().size();
+
+        // Update the player
+        Player updatedPlayer = playerRepository.findById(player.getId()).get();
+        // Disconnect from session so that the updates on updatedPlayer are not directly saved in db
+        em.detach(updatedPlayer);
+
+        restPlayerMockMvc.perform(put("/api/players")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(TestUtil.convertObjectToJsonBytes(updatedPlayer)))
+            .andExpect(status().isOk());
+
+        // Validate the Player in the database
+        List<Player> playerList = playerRepository.findAll();
+        assertThat(playerList).hasSize(databaseSizeBeforeUpdate);
+        Player testPlayer = playerList.get(playerList.size() - 1);
+    }
+
+    @Test
+    @Transactional
+    public void updateNonExistingPlayer() throws Exception {
+        int databaseSizeBeforeUpdate = playerRepository.findAll().size();
+
+        // Create the Player
+
+        // If the entity doesn't have an ID, it will throw BadRequestAlertException
+        restPlayerMockMvc.perform(put("/api/players")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(TestUtil.convertObjectToJsonBytes(player)))
+            .andExpect(status().isBadRequest());
+
+        // Validate the Player in the database
+        List<Player> playerList = playerRepository.findAll();
+        assertThat(playerList).hasSize(databaseSizeBeforeUpdate);
+    }
+
+    @Test
+    @Transactional
+    public void deletePlayer() throws Exception {
+        // Initialize the database
+        playerService.save(player);
+
+        int databaseSizeBeforeDelete = playerRepository.findAll().size();
+
+        // Delete the player
+        restPlayerMockMvc.perform(delete("/api/players/{id}", player.getId())
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isNoContent());
+
+        // Validate the database contains one less item
+        List<Player> playerList = playerRepository.findAll();
+        assertThat(playerList).hasSize(databaseSizeBeforeDelete - 1);
+    }
+}
